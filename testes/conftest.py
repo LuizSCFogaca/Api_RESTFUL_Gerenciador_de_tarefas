be7ea8af5f689678cc0c9ba32676333199117aa3
NiccoFv <nf.vardaramatos@gmail.com>
2026-06-18 01:52:04 -0300

    Corrige configuração dos testes

--- testes/conftest.py
+++ testes/conftest.py
@@ -1,9 +1,13 @@
 import pytest
+import os
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from unittest.mock import patch
+
+os.environ.setdefault("DATABASE_URL", "sqlite:///./test.db")
+
 from main import app
 from backend.database import Base, get_db
 
@@ -30,6 +34,7 @@ app.dependency_overrides[get_db] = override_get_db
 
 @pytest.fixture(scope="function")
 def client():
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     
     with TestClient(app) as c:
